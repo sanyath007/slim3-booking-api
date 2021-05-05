887fcf53b9ac63a47e9a2628e5d37271bbc6afb9
sanyath007 <sanyath007@gmail.com>
2021-05-05 14:24:52 +0700

    Edit RoomController by separating uploadImage method from store mthod and use it in update method and completely modify update method

--- src/Controllers/RoomController.php
+++ src/Controllers/RoomController.php
@@ -75,26 +75,34 @@ class RoomController extends Controller
                 ->write($data);
     }
 
+    private function uploadImage($img, $img_url)
+    {
+        $regx = "/^data:image\/(?<extension>(?:png|gif|jpg|jpeg));base64,(?<image>.+)$/";
+
+        if(preg_match($regx, $img, $matchings)) {
+            $img_data = file_get_contents($img);
+            $extension = $matchings['extension'];
+            $img_name = uniqid().'.'.$extension;
+            $img_full_url = str_replace('/index.php', '/assets/uploads/'.$img_name, $img_url);
+            $file_to_upload = 'assets/uploads/'.$img_name;
+
+            if(file_put_contents($file_to_upload, $img_data)) {
+                return $img_full_url;
+            }
+        }
+
+        return '';
+    }
+
     public function store($request, $response, $args)
     {
-        $post = (array)$request->getParsedBody();
+        $upload_url = 'http://'.$request->getServerParam('SERVER_NAME').$request->getServerParam('PHP_SELF');
 
         try {
-            // TODO: separate uploads to another method
-            /** Upload image */
-            $link = 'http://'.$request->getServerParam('SERVER_NAME').$request->getServerParam('REDIRECT_URL');
-            if(preg_match("/^data:image\/(?<extension>(?:png|gif|jpg|jpeg));base64,(?<image>.+)$/", $post['room_img_url'], $matchings))
-            {
-                $img_data = file_get_contents($post['room_img_url']);
-                $extension = $matchings['extension'];
-                $img_name = uniqid().'.'.$extension;
-                $img_url = str_replace('/rooms', '/assets/uploads/'.$img_name, $link);
-                $file_to_upload = 'assets/uploads/'.$img_name;
+            $post = (array)$request->getParsedBody();
 
-                if(file_put_contents($file_to_upload, $img_data)) {
-                    // echo $img_url;
-                }
-            }
+            /** Upload image */
+            $img_url = $this->uploadImage($post['room_img_url'], $upload_url);
 
             $room = new Room;
             $room->room_no = $post['room_no'];
@@ -108,7 +116,7 @@ class RoomController extends Controller
             $room->room_status = 0;
 
             if($room->save()) {
-                $newRoomId = $room->id;
+                $newRoomId = $room->room_id;
                 $amenities = explode(",", $post['amenities']);
 
                 foreach($amenities as $amenity) {
@@ -119,57 +127,87 @@ class RoomController extends Controller
                     $ra->save();
                 }
 
-                $data = [
-                    'status' => 1,
-                    'message' => 'Insertion successfully!!',
-                    'item' => $room
-                ];
-
                 return $response->withStatus(200)
                         ->withHeader("Content-Type", "application/json")
-                        ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
+                        ->write(json_encode([
+                            'status' => 1,
+                            'message' => 'Insertion successfully!!',
+                            'item' => $room
+                        ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
             } // end if
         } catch (\Throwable $th) {
             /** Delete new room if error occurs */
-            Room::find($newRoomId)->delete();
+            // Room::find($newRoomId)->delete();
             
             /** And set data to client with http status 500 */
-            $data = [
-                'status' => 0,
-                'message' => 'Something went wrong!!'
-            ];
-
             return $response->withStatus(500)
                     ->withHeader("Content-Type", "application/json")
-                    ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
+                    ->write(json_encode([
+                        'status' => 0,
+                        'message' => 'Something went wrong!!'
+                    ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
         } // end trycatch
     }
 
     public function update($request, $response, $args)
     {
-        $post = (array)$request->getParsedBody();
+        $upload_url = 'http://'.$request->getServerParam('SERVER_NAME').$request->getServerParam('PHP_SELF');
 
-        $room = Room::where('id', $args['id'])->first();
-        $room->name = $post['name'];
-        $room->unit = $post['unit'];
-        $room->cost = $post['cost'];
-        $room->stock = $post['stock'];
-        $room->min = $post['min'];
-        $room->balance = $post['balance'];
-        $room->item_type = $post['item_type'];        
-        $room->item_group = $post['item_group'];
+        try {
+            $post = (array)$request->getParsedBody();
 
-        if($room->save()) {   
-            $data = [
-                'status' => 1,
-                'message' => 'Update successfully!!',
-                'item' => $room
-            ];
+            $room = Room::where('room_id', $args['id'])->first();
+            $room->room_no = $post['room_no'];
+            $room->room_name = $post['room_name'];
+            $room->description = $post['description'];
+            $room->room_type = $post['room_type'];
+            $room->room_group = $post['room_group'];
+            $room->building = $post['building'];
+            $room->floor = $post['floor'];
+            $room->room_status = 0;
 
-            return $response->withStatus(200)
+            /** Upload image */
+            $img_url = $this->uploadImage($post['room_img_url'], $upload_url);
+            /** If room_img_url in db is empty and user upload file do this */
+            if(empty($room->room_img_url) && !empty($img_url)) {
+                $room->room_img_url = $img_url;
+            }
+
+            if($room->save()) {
+                $newRoomId = $room->room_id;
+                
+                if(count($post['amenities']) > 0) {
+                    RoomAmenities::where('room_id', $newRoomId)->delete();
+
+                    foreach($post['amenities'] as $amenity) {
+                        $ra = new RoomAmenities;
+                        $ra->room_id = $newRoomId;
+                        $ra->amenity_id = $amenity;
+                        $ra->status = 1;
+                        $ra->save();
+                    }
+                }
+
+                return $response->withStatus(200)
+                        ->withHeader("Content-Type", "application/json")
+                        ->write(json_encode([
+                            'status' => 1,
+                            'message' => 'Update successfully!!',
+                            'item' => $room
+                        ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
+            }
+        } catch (\Throwable $th) {
+            /** Delete new room if error occurs */
+            // Room::find($newRoomId)->delete();
+            
+            /** And set data to client with http status 500 */
+            return $response->withStatus(500)
                     ->withHeader("Content-Type", "application/json")
-                    ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
-        }
+                    ->write(json_encode([
+                        'status' => 0,
+                        'message' => 'Something went wrong!!'
+                    ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT |  JSON_UNESCAPED_UNICODE));
+        } // end trycatch
     }
 
     public function delete($request, $response, $args)
--- src/Models/Room.php
+++ src/Models/Room.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     protected $table = "rooms";
+    protected $primaryKey = "room_id";
 
     public function roomType()
     {
